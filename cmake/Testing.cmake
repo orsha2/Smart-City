0a642151ea96fd25b8a66ca515b5ee26ca045d3b
orsha <or@sha.com>
2026-03-19 12:43:04 +0200

    ET-3: create generic add test function

--- cmake/Testing.cmake
+++ cmake/Testing.cmake
@@ -7,3 +7,15 @@ FetchContent_Declare(
 )
 
 FetchContent_MakeAvailable(unity)
+
+function(add_sc_unit_test test_name source_file tested_target)
+    add_executable(${test_name} ${source_file})
+
+    target_link_libraries(${test_name}
+        PRIVATE
+            ${tested_target}
+            unity
+    )
+
+    add_test(NAME ${test_name} COMMAND $<TARGET_FILE:${test_name}>)
+endfunction()
