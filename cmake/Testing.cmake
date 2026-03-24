0f20a07d6a343f2ba0439892b6d5b31940df7cc8
orsha <or@sha.com>
2026-03-24 12:51:02 +0200

    ET-7: fix build errors

--- cmake/Testing.cmake
+++ cmake/Testing.cmake
@@ -11,7 +11,7 @@ FetchContent_MakeAvailable(unity)
 FetchContent_Declare(
     fff
     GIT_REPOSITORY https://github.com/meekrosoft/fff.git
-    GIT_TAG v1.0
+    GIT_TAG v1.1
     )
     
 FetchContent_MakeAvailable(fff)
@@ -23,7 +23,11 @@ function(add_sc_unit_test test_name source_file tested_target)
         PRIVATE
             ${tested_target}
             unity
-            fff
+    )
+
+    target_include_directories(${test_name}
+        PRIVATE
+            ${fff_SOURCE_DIR}
     )
 
     add_test(NAME ${test_name} COMMAND $<TARGET_FILE:${test_name}>)
--- tests/entry_points/test_entry_points.c
+++ tests/entry_points/test_entry_points.c
@@ -1,23 +1,51 @@
-/** Headers ******************************************************************/
-
 #include "unity.h"
+#include "fff.h"
 
+#include "common.h"
 #include "entry_points.h"
-#include "entry_points_fakes.h"
+#include "subject.h"
+
+DEFINE_FFF_GLOBALS
 
 /** Constants ****************************************************************/
+#define TEST_TEMPERATURE_VALUE     (31U)
+#define TEST_TIME_VALUE            (152311LL)
+#define TEST_TRAFFIC_VALUE         (77U)
+#define TEST_RED_LIGHT_DURATION    (45U)
+#define TEST_GREEN_LIGHT_DURATION  (20U)
 
-#define TEST_TEMPERATURE_VALUE      (31U)
-#define TEST_TIME_VALUE             (152311LL)
-#define TEST_TRAFFIC_VALUE          (77U)
-#define TEST_RED_LIGHT_DURATION     (45U)
-#define TEST_GREEN_LIGHT_DURATION   (20U)
+/** Fakes ********************************************************************/
+
+FAKE_VALUE_FUNC0(uint32_t, HAL_read_temperature_sensor)
+FAKE_VALUE_FUNC0(int64_t, HAL_read_time_sensor)
+FAKE_VALUE_FUNC0(uint16_t, HAL_read_traffic_sensor)
+
+FAKE_VOID_FUNC3(HAL_update_air_conditioner_state,
+                enum HAL_AIR_CONDITIONER_STATE_e,
+                enum HAL_AIR_CONDITIONER_MODE_e,
+                enum HAL_AIR_CONDITIONER_FAN_LEVEL_e)
+
+FAKE_VOID_FUNC0(HAL_turn_on_streetlights)
+FAKE_VOID_FUNC0(HAL_turn_off_streetlights)
+FAKE_VOID_FUNC2(HAL_set_traffic_light, size_t, size_t)
+
+FAKE_VALUE_FUNC2(enum status_e, SUBJECT_notify_all, struct Subject_s *, void *)
 
 /** Functions ****************************************************************/
-
 void setUp(void)
 {
-    ENTRY_POINTS_FAKES_reset();
+    RESET_FAKE(HAL_read_temperature_sensor);
+    RESET_FAKE(HAL_read_time_sensor);
+    RESET_FAKE(HAL_read_traffic_sensor);
+    RESET_FAKE(HAL_update_air_conditioner_state);
+    RESET_FAKE(HAL_turn_on_streetlights);
+    RESET_FAKE(HAL_turn_off_streetlights);
+    RESET_FAKE(HAL_set_traffic_light);
+    RESET_FAKE(SUBJECT_notify_all);
+
+    FFF_RESET_HISTORY();
+
+    SUBJECT_notify_all_fake.return_val = SC_STATUS_SUCCESS;
 }
 
 void tearDown(void)
@@ -59,53 +87,65 @@ void test_traffic_entry_point_should_fail_when_self_is_null(void)
 
 void test_temperature_entry_point_should_read_temperature_from_hal_and_notify_subject(void)
 {
-    struct Subject_s subject;
+    struct Subject_s subject = {0};
+    struct TemperatureRawData_s *raw_data = NULL;
     enum status_e status = SC_STATUS_UNINITIALIZED;
 
     HAL_read_temperature_sensor_fake.return_val = TEST_TEMPERATURE_VALUE;
-    SUBJECT_notify_all_fake.custom_fake = naive_temperature_notify_all_fake;
 
     status = temperature_entry_point(&subject);
 
     TEST_ASSERT_EQUAL_INT(SC_STATUS_SUCCESS, status);
     TEST_ASSERT_EQUAL_UINT(1U, HAL_read_temperature_sensor_fake.call_count);
     TEST_ASSERT_EQUAL_UINT(1U, SUBJECT_notify_all_fake.call_count);
-    TEST_ASSERT_EQUAL_PTR(&subject, g_subject_seen);
-    TEST_ASSERT_EQUAL_UINT(TEST_TEMPERATURE_VALUE, g_temperature_raw_data.value);
+
+    TEST_ASSERT_EQUAL_PTR(&subject, SUBJECT_notify_all_fake.arg0_val);
+
+    raw_data = (struct TemperatureRawData_s *)SUBJECT_notify_all_fake.arg1_val;
+    TEST_ASSERT_NOT_NULL(raw_data);
+    TEST_ASSERT_EQUAL_UINT(TEST_TEMPERATURE_VALUE, raw_data->value);
 }
 
 void test_time_entry_point_should_read_time_from_hal_and_notify_subject(void)
 {
-    struct Subject_s subject;
+    struct Subject_s subject = {0};
+    struct TimeRawData_s *raw_data = NULL;
     enum status_e status = SC_STATUS_UNINITIALIZED;
 
     HAL_read_time_sensor_fake.return_val = TEST_TIME_VALUE;
-    SUBJECT_notify_all_fake.custom_fake = naive_time_notify_all_fake;
 
     status = time_entry_point(&subject);
 
     TEST_ASSERT_EQUAL_INT(SC_STATUS_SUCCESS, status);
     TEST_ASSERT_EQUAL_UINT(1U, HAL_read_time_sensor_fake.call_count);
     TEST_ASSERT_EQUAL_UINT(1U, SUBJECT_notify_all_fake.call_count);
-    TEST_ASSERT_EQUAL_PTR(&subject, g_subject_seen);
-    TEST_ASSERT_EQUAL_INT64(TEST_TIME_VALUE, g_time_raw_data.value);
+
+    TEST_ASSERT_EQUAL_PTR(&subject, SUBJECT_notify_all_fake.arg0_val);
+
+    raw_data = (struct TimeRawData_s *)SUBJECT_notify_all_fake.arg1_val;
+    TEST_ASSERT_NOT_NULL(raw_data);
+    TEST_ASSERT_EQUAL_INT64(TEST_TIME_VALUE, raw_data->value);
 }
 
 void test_traffic_entry_point_should_read_traffic_from_hal_and_notify_subject(void)
 {
-    struct Subject_s subject;
+    struct Subject_s subject = {0};
+    struct TrafficRawData_s *raw_data = NULL;
     enum status_e status = SC_STATUS_UNINITIALIZED;
 
     HAL_read_traffic_sensor_fake.return_val = TEST_TRAFFIC_VALUE;
-    SUBJECT_notify_all_fake.custom_fake = naive_traffic_notify_all_fake;
 
     status = traffic_entry_point(&subject);
 
     TEST_ASSERT_EQUAL_INT(SC_STATUS_SUCCESS, status);
     TEST_ASSERT_EQUAL_UINT(1U, HAL_read_traffic_sensor_fake.call_count);
     TEST_ASSERT_EQUAL_UINT(1U, SUBJECT_notify_all_fake.call_count);
-    TEST_ASSERT_EQUAL_PTR(&subject, g_subject_seen);
-    TEST_ASSERT_EQUAL_UINT(TEST_TRAFFIC_VALUE, g_traffic_raw_data.value);
+
+    TEST_ASSERT_EQUAL_PTR(&subject, SUBJECT_notify_all_fake.arg0_val);
+
+    raw_data = (struct TrafficRawData_s *)SUBJECT_notify_all_fake.arg1_val;
+    TEST_ASSERT_NOT_NULL(raw_data);
+    TEST_ASSERT_EQUAL_UINT(TEST_TRAFFIC_VALUE, raw_data->value);
 }
 
 void test_city_api_cooling_entry_point_should_fail_when_self_is_null(void)
@@ -141,7 +181,7 @@ void test_city_api_traffic_entry_point_should_fail_when_self_is_null(void)
 
 void test_city_api_cooling_entry_point_should_call_hal_update_air_conditioner_state(void)
 {
-    struct CoolingCommand_s command;
+    struct CoolingCommand_s command = {0};
     enum status_e status = SC_STATUS_UNINITIALIZED;
 
     command.state = HAL_AIR_CONDITIONER_STATE_ON;
@@ -152,17 +192,14 @@ void test_city_api_cooling_entry_point_should_call_hal_update_air_conditioner_st
 
     TEST_ASSERT_EQUAL_INT(SC_STATUS_SUCCESS, status);
     TEST_ASSERT_EQUAL_UINT(1U, HAL_update_air_conditioner_state_fake.call_count);
-    TEST_ASSERT_EQUAL_INT(command.state,
-                          HAL_update_air_conditioner_state_fake.arg0_val);
-    TEST_ASSERT_EQUAL_INT(command.mode,
-                          HAL_update_air_conditioner_state_fake.arg1_val);
-    TEST_ASSERT_EQUAL_INT(command.fan_level,
-                          HAL_update_air_conditioner_state_fake.arg2_val);
+    TEST_ASSERT_EQUAL_INT(command.state, HAL_update_air_conditioner_state_fake.arg0_val);
+    TEST_ASSERT_EQUAL_INT(command.mode, HAL_update_air_conditioner_state_fake.arg1_val);
+    TEST_ASSERT_EQUAL_INT(command.fan_level, HAL_update_air_conditioner_state_fake.arg2_val);
 }
 
 void test_city_api_lighting_entry_point_should_turn_on_streetlights_when_requested(void)
 {
-    struct LightingCommand_s command;
+    struct LightingCommand_s command = {0};
     enum status_e status = SC_STATUS_UNINITIALIZED;
 
     command.should_turn_on = true;
@@ -176,7 +213,7 @@ void test_city_api_lighting_entry_point_should_turn_on_streetlights_when_request
 
 void test_city_api_lighting_entry_point_should_turn_off_streetlights_when_requested(void)
 {
-    struct LightingCommand_s command;
+    struct LightingCommand_s command = {0};
     enum status_e status = SC_STATUS_UNINITIALIZED;
 
     command.should_turn_on = false;
@@ -190,7 +227,7 @@ void test_city_api_lighting_entry_point_should_turn_off_streetlights_when_reques
 
 void test_city_api_traffic_entry_point_should_call_hal_set_traffic_light(void)
 {
-    struct TrafficCommand_s command;
+    struct TrafficCommand_s command = {0};
     enum status_e status = SC_STATUS_UNINITIALIZED;
 
     command.red_light_duration = TEST_RED_LIGHT_DURATION;
@@ -200,10 +237,8 @@ void test_city_api_traffic_entry_point_should_call_hal_set_traffic_light(void)
 
     TEST_ASSERT_EQUAL_INT(SC_STATUS_SUCCESS, status);
     TEST_ASSERT_EQUAL_UINT(1U, HAL_set_traffic_light_fake.call_count);
-    TEST_ASSERT_EQUAL_UINT(TEST_RED_LIGHT_DURATION,
-                           HAL_set_traffic_light_fake.arg0_val);
-    TEST_ASSERT_EQUAL_UINT(TEST_GREEN_LIGHT_DURATION,
-                           HAL_set_traffic_light_fake.arg1_val);
+    TEST_ASSERT_EQUAL_UINT(TEST_RED_LIGHT_DURATION, HAL_set_traffic_light_fake.arg0_val);
+    TEST_ASSERT_EQUAL_UINT(TEST_GREEN_LIGHT_DURATION, HAL_set_traffic_light_fake.arg1_val);
 }
 
 int main(void)
@@ -213,12 +248,15 @@ int main(void)
     RUN_TEST(test_temperature_entry_point_should_fail_when_self_is_null);
     RUN_TEST(test_time_entry_point_should_fail_when_self_is_null);
     RUN_TEST(test_traffic_entry_point_should_fail_when_self_is_null);
+
     RUN_TEST(test_temperature_entry_point_should_read_temperature_from_hal_and_notify_subject);
     RUN_TEST(test_time_entry_point_should_read_time_from_hal_and_notify_subject);
     RUN_TEST(test_traffic_entry_point_should_read_traffic_from_hal_and_notify_subject);
+
     RUN_TEST(test_city_api_cooling_entry_point_should_fail_when_self_is_null);
     RUN_TEST(test_city_api_lighting_entry_point_should_fail_when_self_is_null);
     RUN_TEST(test_city_api_traffic_entry_point_should_fail_when_self_is_null);
+
     RUN_TEST(test_city_api_cooling_entry_point_should_call_hal_update_air_conditioner_state);
     RUN_TEST(test_city_api_lighting_entry_point_should_turn_on_streetlights_when_requested);
     RUN_TEST(test_city_api_lighting_entry_point_should_turn_off_streetlights_when_requested);
